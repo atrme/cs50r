cf92a02bdf124cbbd836444d2fec4f7765f8464a
atrme <ischaoyangy@gmail.com>
2024-12-19 17:23:53 +0800

    Delete final project directory

--- my_code/final_project/project/tests/testthat/test-say_ciallo.R
+++
@@ -1,8 +0,0 @@
-describe("say_ciallo()", {
-  it("can say ciallo", {
-    expect_match(say_ciallo(), "Ciallo~")
-  })
-  it("can say ciallo with a sensei emoji", {
-    expect_match(say_ciallo(), "(∠◠ڼ◠)⌒☆", fixed = TRUE)
-  })
-})
--- my_code/final_project/project/tests/testthat/test-say_electrify.R
+++
@@ -1,9 +0,0 @@
-describe("say_electrify()", {
-  it("can electrify", {
-    expect_match(say_electrify(), "对着")
-    expect_match(say_electrify(), "冲了")
-  })
-  it("can electrify with a sensei emoji", {
-    expect_match(say_electrify(), "ᕕ(◠ڼ◠)ᕗ", fixed = TRUE)
-  })
-})
